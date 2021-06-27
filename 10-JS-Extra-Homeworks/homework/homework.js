8ad5364a37c66050a20d42b382b63b5888b0f307
mercedes Andreucci <avallemiguel@hotmail.com>
2021-06-26 21:49:47 -0300

    extras

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var matriz = new Array();
+  for (clave in objeto){
+    matriz.push([clave, objeto[clave]]);
+  }
+  return matriz;
 }
 
 
@@ -18,6 +23,16 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var array = new Array();
+  var object = {};
+  array = string.split("");
+  array.forEach(element =>{
+    if (!object[element]){
+      object[element]=0;
+    }
+    object[element] ++;
+  });
+  return object
 }
 
 
@@ -26,7 +41,20 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
-}
+  var array = new Array();
+  var may = new Array();
+  var min = new Array();
+  array = s.split("");
+  array.forEach(element =>{
+    if (element.toUpperCase() === element){
+      may.push(element);
+    }else{
+      min.push(element);
+    }
+  });
+  s = may.join("") + min.join("");
+  return s;
+ }
 
 
 function asAmirror(str) {
@@ -35,6 +63,22 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var alt = new Array();
+  var array = new Array();
+  array = str.split("");
+  str = "";
+  array.forEach(element=>{
+    if (element !== " "){
+      alt.unshift(element);
+    }else{
+      str += alt.join("");
+      str += " ";
+      alt = [];
+    }
+  });
+  str += alt.join("");
+ // console.log(str);
+  return str;
 } 
 
 
@@ -43,6 +87,18 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var str = numero.toString(10);
+  var contador = 0;
+  do{
+   // console.log(str.charAt(contador));
+   // console.log(str.charAt(str.length - 1 - contador));
+    if (str.charAt(contador) === str.charAt(str.length - 1 - contador)){
+      contador ++;
+    }else{
+      return "No es capicua";
+    }
+  }while (contador <= (str.length / 2));
+  return "Es capicua";
 }
 
 
@@ -50,6 +106,15 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var modificada = "";
+  var contador = 0;
+  do{
+    if ((cadena.charAt(contador)!== "a") && (cadena.charAt(contador)!== "b") && (cadena.charAt(contador)!== "c")){
+      modificada += cadena.charAt(contador);
+    }
+    contador ++;
+  }while (contador <= cadena.length - 1);
+  return modificada;
 }
 
 
@@ -57,6 +122,18 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  var str = "";
+  for (i = 0; i <= arr.length - 1; i++){
+    for (x = i + 1; x <= arr.length - 1; x++){
+      if (arr[x].length < arr[i].length){
+        str = arr[i];
+        arr[i] = arr[x];
+        arr[x] = str; 
+      }
+    }
+  }
+  
+  return arr;
 }
 
 
@@ -65,7 +142,11 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí 
+  var nuevoarray = new Array();
+  nuevoarray = arreglo1.filter(element => (arreglo2.find(el => el === element)));
+  console.log(nuevoarray);
+  return nuevoarray; 
 }
 
 
